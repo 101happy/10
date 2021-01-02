4f98de8fd356e4a8c9e83f645ee8b987f6766c3c
101happy <76813843+101happy@users.noreply.github.com>
2021-01-02 15:02:41 +0200

    Add files via upload

---
+++ 10.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+int num1;
+ cout <<  "Enter a number: ";
+ cin >> num1;
+ if (num1 > 10){
+    cout << "The number is larger then 10.";
+ } else if (num1 < 10)
+ {
+     cout << "The number is smaller then 10.";
+ } else if (num1 = 10){
+     cout << "The number is equal to 10.";
+ }
+}
